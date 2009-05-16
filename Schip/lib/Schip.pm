f42883258744966f5ff58a2ff1d85c658f2785e2
John Berthels <john@louse.(none)>
2009-05-16 15:29:27 +0100

    remove h2xs default cruft

--- Schip/lib/Schip.pm
+++ Schip/lib/Schip.pm
@@ -4,73 +4,32 @@ use 5.010000;
 use strict;
 use warnings;
 
-require Exporter;
-
-our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Schip ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
-
 our $VERSION = '0.01';
 
 
-# Preloaded methods go here.
-
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
-Schip - Perl extension for blah blah blah
+Schip - Scheme-in-perl
 
 =head1 SYNOPSIS
 
   use Schip;
-  blah blah blah
 
 =head1 DESCRIPTION
 
-Stub documentation for Schip, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
 
 =head2 EXPORT
 
 None by default.
 
-
-
 =head1 SEE ALSO
-
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
 
 =head1 AUTHOR
 
-John Berthels, E<lt>john@E<gt>
+John Berthels, E<lt>jjberthels@gmail.com<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
